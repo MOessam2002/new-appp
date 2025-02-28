6082887a8c5f8ea085ebc3efa7d8de35c4e9883d
mohamed <mohamed@192.168.158.131>
2025-02-27 19:55:41 -0500

    my new project

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HomePage from './components/HomePage';
+import UsersList from './components/UsersList';
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+      <UsersList />
+    </div>
+  );
+}
+
+export default App;
